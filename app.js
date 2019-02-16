731bf4de0dbb5f9806baa51a4a25ef3ca98df220
Ahmedten <microtop2013@gmail.com>
2019-02-16 20:40:03 +0200

    Donuts Revisited Quiz

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-alert ("Hello Ahmed");
+// * Programming Quiz: Donuts Revisited (7-6)
 
-console.log(" First Code");
+
+var donuts = [
+   { type: "Jelly", cost: 1.22 },
+   { type: "Chocolate", cost: 2.45 },
+   { type: "Cider", cost: 1.59 },
+   { type: "Boston Cream", cost: 5.99 }
+];
+
+donuts.forEach(donut =>{
+   console.log(`${donut.type} donuts cost $${donut.cost} each`);
+});
+
